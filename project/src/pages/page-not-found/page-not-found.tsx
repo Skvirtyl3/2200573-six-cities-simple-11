7aabc4ea6a569c429f9873a3866d69d09d6df195
Roman Filchev <skvirtyl3@gmail.com>
2022-10-31 01:46:03 +0500

    Добавил маршрутизацию, Logo, приватный маршрут, helmet

--- project/src/pages/page-not-found/page-not-found.tsx
+++ project/src/pages/page-not-found/page-not-found.tsx
@@ -1,14 +1,15 @@
-import Logo from '../../components/logo/logo';
 import { Helmet } from 'react-helmet-async';
+import Logo from '../../components/logo/logo';
 
-function MainEmpty(): JSX.Element
+function PageNotFound(): JSX.Element
 {
   return(
-    <div className="page page--gray page--main">
-      <Helmet><title>Шесть городов. Предложения не найдены.</title></Helmet>
+    <body className="page page--gray page--main">
+      <Helmet><title>Шесть городов. Страница не найдена.</title></Helmet>
       <div style={{display: 'none'}}>
         <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
       </div>
+
       <Logo />
       <main className="page__main page__main--index page__main--index-empty">
         <h1 className="visually-hidden">Cities</h1>
@@ -49,18 +50,12 @@ function MainEmpty(): JSX.Element
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
-              </div>
-            </section>
-            <div className="cities__right-section"></div>
+          <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
+            <span>404 Not Found</span>
           </div>
         </div>
       </main>
-    </div>);
+    </body>);
 }
 
-export default MainEmpty;
+export default PageNotFound;
