19fbd31884b1b31548cf994a2636d672793d6c40
Roman Filchev <skvirtyl3@gmail.com>
2022-11-13 21:28:21 +0500

    Revert "5.2 - реализовал выделение точек на карте на странице предложения"
    This reverts commit 112b3c9cd8f64895f46cc5f377169474c6fc3101.

--- project/src/components/offers-other/offers-other.tsx
+++ project/src/components/offers-other/offers-other.tsx
@@ -6,10 +6,9 @@ import Offer from '../offer/offer';
 type OffersOtherProps = {
   offerParameters: OfferParameter[];
   currentOfferKey: string; //ид текущего отображаемого оффера, исключаем его из перечня, чтобы в блоке "Other places in the neighbourhood" он не отобразился
-  onMouseEnter: (id:string) => void;
 }
 
-function OffersOther({offerParameters, currentOfferKey, onMouseEnter}:OffersOtherProps) : JSX.Element
+function OffersOther({offerParameters,currentOfferKey}:OffersOtherProps) : JSX.Element
 {
   const otherOffer = offerParameters.filter((item) => item.key !== currentOfferKey);
   return(
@@ -17,7 +16,7 @@ function OffersOther({offerParameters, currentOfferKey, onMouseEnter}:OffersOthe
       <h2 className="near-places__title">Other places in the neighbourhood</h2>
       <div className="near-places__list places__list">
         {otherOffer.slice(0, NEAR_PLACES_COUNT).map((item) =>
-          <Offer key={item.key} offerParameter={item} onMouseEnter={onMouseEnter} />
+          <Offer key={item.key} offerParameter={item} />
         )}
       </div>
     </section>
