4a909ccc4495a8fe0cb38d7ffd7e79a6f40e082e
Roman Filchev <skvirtyl3@gmail.com>
2022-11-28 19:05:30 +0500

    7.2 - реализация авторизации в системе

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
+import LoginForm from '../../components/login-form/login-form';
 
 function Login() : JSX.Element
 {
@@ -9,20 +10,7 @@ function Login() : JSX.Element
       <Header isNumberCitiesLogo isLogoLinkActive={false} isHaveHeaderNav={false}/>
       <main className="page__main page__main--login">
         <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
-          </section>
+          <LoginForm/>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
               <a className="locations__item-link" href="\#">
