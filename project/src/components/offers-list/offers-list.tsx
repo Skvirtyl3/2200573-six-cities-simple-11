474bc5a152258d5103c4e05cc1b17edd9d45639c
Roman Filchev <skvirtyl3@gmail.com>
2022-12-03 23:43:15 +0500

    8 - обработка ошибки при загрузке страницы списка предложений

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -5,6 +5,9 @@ import OfferOrder from '../offer-order/offer-order';
 import { Location } from '../../types/location';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setHoverOfferPoint } from '../../store/offer-search-process/offer-search-process';
+import { fetchHotelsAction } from '../../store/api-actions';
+import { getErrorStatus } from '../../store/offer-search-data/selectors';
+import ErrorScreen from '../error-screen/error-screen';
 
 type OffersListProps = {
   offerParameters: OfferInfo[];
@@ -12,13 +15,22 @@ type OffersListProps = {
 
 function OffersList({offerParameters}:OffersListProps): JSX.Element
 {
+  const hasError = useAppSelector(getErrorStatus);
   const dispatch = useAppDispatch();
+  const city = useAppSelector(selectFilterCity);
 
   function handleOfferMouseEnter(point: Location | undefined | null): void {
     dispatch(setHoverOfferPoint(point));
   }
 
-  const city = useAppSelector(selectFilterCity);
+  if (hasError) {
+    return (
+      <ErrorScreen errorText='Не удалось загрузить информацию по списку предложений' onClick={() => {
+        dispatch(fetchHotelsAction());
+      }}
+      />);
+  }
+
   return(
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
